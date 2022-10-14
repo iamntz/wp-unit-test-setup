3596b168ecb65f02dd114e3533bf7df659dfc2c6
Ionuț Staicu <github@iamntz.com>
2022-10-14 08:47:22 +0300

    Add php-mock

--- unit/SampleWithMockingTest.php
+++ unit/SampleWithMockingTest.php
@@ -3,10 +3,13 @@
 namespace tests\unit;
 
 use Brain\Monkey\Functions;
+use phpmock\spy\Spy;
 use PHPUnit\Framework\TestCase;
 
 class SampleWithMockingTest extends TestCase
 {
+    use \phpmock\phpunit\PHPMock;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -26,5 +29,18 @@ class SampleWithMockingTest extends TestCase
     public function testFoo()
     {
         $this->assertTrue(true);
+    }
+
+    public function testSpyFunction()
+    {
+        // this namespace should match your code namespace.
+        // your code MUST be under a namespace, otherwise spying/mocking won't work
+        // https://github.com/php-mock/php-mock
+        $spy = new Spy('my\namespace', "wp_insert_post", fn() => func_get_args());
+        $spy->enable();
+
+        // execute your code here
+
+        $this->assertCount(1, $spy->getInvocations());
     }
 }
